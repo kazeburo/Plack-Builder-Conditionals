ac7407708c12a268efe7b8022bb1c0b81e36b072
Masahiro Nagano <kazeburo@gmail.com>
2010-07-14 23:50:06 +0900

    initial commit

---
+++ lib/Plack/Builder/Conditions.pm
@@ -0,0 +1,32 @@
+package Plack::Builder::Conditions;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Builder::Conditions -
+
+=head1 SYNOPSIS
+
+  use Plack::Builder::Conditions;
+
+=head1 DESCRIPTION
+
+Plack::Builder::Conditions is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
